87703b875aea6a5ad9aca2fa868c5fbb1f84a225
Muhammad Asharul <masharul51@gmail.com>
2024-09-19 21:06:47 +0700

    add: note found page

--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,3 +1,7 @@
+import { notFound } from "next/navigation";
 export default function Note({ params }: { params: { noteId: string } }) {
+  if (parseInt(params.noteId) > 100) {
+    notFound();
+  }
   return <h1>Note Detail {params.noteId}</h1>;
 }
