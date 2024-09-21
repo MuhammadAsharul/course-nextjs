00ae1808717f576ef3089996e9dc302d12c41e21
Muhammad Asharul <masharul51@gmail.com>
2024-09-21 19:53:35 +0700

    add : metadata seo

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page",
+  description: "this is about page",
+};
+
+export default function Page() {
+  return <h2>About Page</h2>;
+}
