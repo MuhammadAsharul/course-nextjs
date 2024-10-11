8a5527edfc7155d8c4aac9effb55b5502dd3e01e
Muhammad Asharul <masharul51@gmail.com>
2024-10-11 20:44:49 +0700

    add: handling post request

--- src/app/posts/route.ts
+++ src/app/posts/route.ts
@@ -1,5 +1,19 @@
-import { post } from "./data";
+import { posts } from "./data";
 export async function GET() {
   //   return new Response("Hello from Route Post");
-  return Response.json(post);
+  return Response.json(posts);
+}
+export async function POST(request: Request) {
+  const post = await request.json();
+  const newPost = {
+    id: posts.length + 1,
+    ...post,
+  };
+  posts.push(newPost);
+  return Response.json(newPost, {
+    status: 201,
+  });
+
+  //   console.log(post);
+  //   return new Response("post request");
 }
