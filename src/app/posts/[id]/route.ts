fcbcf8bdb49e83942e2bef5c705fe545ad71ce82
Muhammad Asharul <masharul51@gmail.com>
2024-10-12 21:19:45 +0700

    add: handling PUT & PATCH request

--- src/app/posts/[id]/route.ts
+++ src/app/posts/[id]/route.ts
@@ -8,3 +8,25 @@ export async function GET(
   const post = posts.find((p) => p.id === parseInt(params.id));
   return NextResponse.json(post);
 }
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { text } = await request.json();
+  const index = posts.findIndex((post) => post.id === parseInt(params.id));
+
+  posts[index].text = text;
+  return NextResponse.json(posts[index]);
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const index = posts.findIndex((post) => post.id === parseInt(params.id));
+
+  posts[index] = { id: parseInt(params.id), ...body };
+  return NextResponse.json(posts[index]);
+}
