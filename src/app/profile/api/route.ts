f53e7ee74a9edaaeda7eb2dc4216a07cae4bee64
Muhammad Asharul <masharul51@gmail.com>
2024-10-13 20:47:46 +0700

    add: cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,13 +1,18 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   //   const headerList = new Headers(request.headers);
   const headerList = headers();
   console.log(headerList.get("Authorization"));
+  console.log(request.cookies.get("token"));
+
+  cookies().set("theme", "dark");
+  console.log(cookies().get("theme"));
   return new Response("<h1>Profile data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "token=test123",
     },
   });
 }
